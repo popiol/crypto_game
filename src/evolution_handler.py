b229693c5e084ac4d24794fc0ec0b6213873d7bc
popiol <opiola.piotr@gmail.com>
2024-05-05 22:47:35 +0200

    evolution

--- src/evolution_handler.py
+++ src/evolution_handler.py
@@ -74,28 +74,8 @@ class EvolutionHandler:
         l = keras.layers.Dense(self.OUTPUT_SIZE, name="output")(l)
         outputs = l
         model = keras.Model(inputs=inputs, outputs=outputs, name=f"{model_id_1}_{model_id_2}")
-        model.compile(optimizer=optimizers.Adam(learning_rate=0.001), loss="mean_squared_error")
+        model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss="mean_squared_error")
         return model
-
-    def add_layer(self, inputs, old_layer, new_layer, *args, **kwargs):
-        model_id = old_layer.name.split("_")[-1]
-        l = self.stacks.get(model_id, inputs)
-        if model_id == "output":
-            l = list(self.stacks.values())[0]
-            self.stacks = {}
-        elif old_layer.name.startswith("concatenate"):
-            model_ids = old_layer.name.split("_")[1:3]
-            l = [self.stacks[x] for x in model_ids]
-            for model_id2 in model_ids:
-                del self.stacks[model_id2]
-            kwargs["name"] = (
-                "concatenate_" + "_".join([self.new_model_id(x) for x in model_ids]) + "_" + self.new_model_id(model_id)
-            )
-        if "name" not in kwargs:
-            kwargs["name"] = old_layer.name.split("_")[0] + "_" + self.new_model_id(model_id)
-        l = new_layer(*args, **kwargs, **self.get_initializers(old_layer))(l)
-        self.stacks[model_id] = l
-        return l
 
     def copy_structure(self, model, inputs=None, new_output=False):
         self.stacks = {}
@@ -131,3 +111,23 @@ class EvolutionHandler:
                     l = self.add_layer(inputs, layer, keras.layers.Dense, layer.output_shape[-1], name="output")
                     outputs = l
         return inputs, hidden, outputs
+
+    def add_layer(self, inputs, old_layer, new_layer, *args, **kwargs):
+        model_id = old_layer.name.split("_")[-1]
+        l = self.stacks.get(model_id, inputs)
+        if model_id == "output":
+            l = list(self.stacks.values())[0]
+            self.stacks = {}
+        elif old_layer.name.startswith("concatenate"):
+            model_ids = old_layer.name.split("_")[1:3]
+            l = [self.stacks[x] for x in model_ids]
+            for model_id2 in model_ids:
+                del self.stacks[model_id2]
+            kwargs["name"] = (
+                "concatenate_" + "_".join([self.new_model_id(x) for x in model_ids]) + "_" + self.new_model_id(model_id)
+            )
+        if "name" not in kwargs:
+            kwargs["name"] = old_layer.name.split("_")[0] + "_" + self.new_model_id(model_id)
+        l = new_layer(*args, **kwargs, **self.get_initializers(old_layer))(l)
+        self.stacks[model_id] = l
+        return l
