2b20a1f437e21d1a7ef4a42490f60544ab5d0e7c
popiol <opiola.piotr@gmail.com>
2024-06-23 17:18:05 +0200

    test is price

--- test/test_transformer.py
+++ test/test_transformer.py
@@ -8,3 +8,7 @@ class TestTransformer:
         features.set_feature("closing_price", 1.1)
         v = features.to_vector()
         assert v[6] == 1.1
+
+    def test_is_price(self):
+        assert InputFeatures.is_price(6)
+        assert not InputFeatures.is_price(8)
