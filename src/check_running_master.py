146980bbb3338568084bdfd1c7100602487f5060
popiol <opiola.piotr@gmail.com>
2024-07-18 23:52:57 +0200

    evaluate

--- src/check_running_master.py
+++ src/check_running_master.py
@@ -1,7 +1,13 @@
+import sys
+
 import psutil
 
 process_name = "src.rl_runner"
-processes = [p.cmdline() for p in psutil.process_iter() if len(p.cmdline()) > 2 and p.cmdline()[2] == process_name]
+processes = [
+    p.cmdline()
+    for p in psutil.process_iter()
+    if len(p.cmdline()) > 2 and p.cmdline()[2] == process_name and p.cmdline()[3:] == sys.argv[1:]
+]
 if len(processes) > 1:
     print("Master process up and running")
     exit()
