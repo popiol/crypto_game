506f3c0f7e96ea6c53bf7e6cbcdc7787fb201c48
popiol <opiola.piotr@gmail.com>
2024-07-28 23:06:00 +0200

    monitor

--- src/webapp/pages/current_metrics.py
+++ src/webapp/pages/current_metrics.py
@@ -1,12 +1,49 @@
+import glob
 import json
+import os
 
+import pandas as pd
 import streamlit as st
 
 from src.rl_runner import RlRunner
 
 st.set_page_config(page_title="Current metrics")
 
-st.title("Current metrics")
+model_name = st.query_params.get("model_name")
+
+
+def list_current_models():
+    st.title("Current metrics")
+    eval_file_path = "logs/evaluate.log"
+    if not os.path.exists(eval_file_path):
+        eval_file_path = max(glob.glob("logs/archive/evaluate_*.log"))
+    with open(eval_file_path) as f:
+        log = f.read()
+    pos1 = log.index("Evaluate models")
+    pos1 = log.index("\n", pos1) + 1
+    pos2 = log.index("archive") - 1
+    lines = log[pos1:pos2].splitlines()
+    scores = [line.split()[2:] for line in lines]
+    scores = sorted(scores, key=lambda x: -float(x[1]))
+    for row in scores:
+        row[0] = f"?model_name={row[0]}"
+    df = pd.DataFrame(scores, columns=["model", "score"])
+    st.dataframe(
+        df, hide_index=True, column_config={"model": st.column_config.LinkColumn(), "score": st.column_config.NumberColumn()}
+    )
+
+
+def show_model(model_name: str):
+    st.title(model_name)
+
+
+if model_name:
+    show_model(model_name)
+else:
+    list_current_models()
+
+
+exit()
 
 rl_runner = RlRunner()
 rl_runner.load_config("config/config.yml")
