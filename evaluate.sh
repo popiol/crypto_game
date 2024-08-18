84c5705b946094be86d52f671d20975adbb7f3a6
popiol <opiola.piotr@gmail.com>
2024-08-18 12:54:44 +0200

    fix

--- evaluate.sh
+++ evaluate.sh
@@ -14,4 +14,4 @@ git pull
 mkdir -p logs/archive
 timestamp=$(date '+%Y%m%d%H%M%S')
 mv logs/evaluate.log logs/archive/evaluate_$timestamp.log 2> /dev/null
-python -m src.rl_runner --config config/config.yml --evaluate > logs/evaluate.log 2> logs/evaluate.log
+python -m src.rl_runner --evaluate > logs/evaluate.log 2> logs/evaluate.log
