57a3c294050381107b304796379eba882f205ddc
popiol <opiola.piotr@gmail.com>
2025-08-01 13:03:59 +0200

    fix sell price

--- src/agent.py
+++ src/agent.py
@@ -52,7 +52,7 @@ class Agent:
         for position in portfolio.positions:
             sell_price = quotes.closing_price(position.asset) * output[position.asset].relative_sell_price
             if position.last_sell_price is not None:
-                sell_price = sell_price * 0.1 + position.last_sell_price * 0.9
+                sell_price = max(sell_price * 0.1 + position.last_sell_price * 0.9, quotes.closing_price(position.asset))
             position.last_sell_price = sell_price
             sell_order = PortfolioOrder(
                 order_type=PortfolioOrderType.sell,
